5795d76a267e0be0ba2709750d8dcd2cf766fad4
zie zie <zie93zie@gmail.com>
2024-08-08 13:45:37 +0200

    chenge modal

--- todolist/src/components/detailsList.vue
+++ todolist/src/components/detailsList.vue
@@ -1,10 +1,14 @@
 <template>
-  <div>
-    <p v-if="list" :key="list._id" class="list">
-      {{ list.description }}
-    </p>
-    <p v-else>Loading...</p>
+<div>
+    <div v-if="list&&showmodal" :key="list._id" >
+      <div class="modal" >
+        <div><button @click="showmodal=!showmodal" class="closemodalbuttom">X</button></div>
+        <h2>Details</h2>
+      <p>{{ list.description }}</p>
+    </div>
   </div>
+
+</div>
 </template>
 
 <script>
@@ -14,6 +18,7 @@ export default {
   data() {
     return {
       list: null,
+      showmodal:true
     };
   },
   watch: {
@@ -30,12 +35,44 @@ export default {
         .then((res) => res.json())
         .then((data) => {
           this.list = data.todos.find(item => item._id === id);
+          console.log('Fetched data:',this.list );
         })
         .catch((err) => console.error('Error fetching data:', err));
     },
+    
+    
   },
 };
 </script>
 <style>
+.modal {
+  background: rgb(211, 233, 235);
+  padding: 20px;
+  border-radius: 8px;
+  width: 400px;
+  max-width: 100%;
+  text-align: center;
+  z-index: 1;
+  position: fixed; 
+  left: 50%;
+  transform: translate(-50%, -50%); 
+}
 
+.closemodal {
+  color: rgb(17, 20, 22);
+  width: 30px;
+  height: 30px;
+  background: white;
+  border: none;
+  font-size: 18px;
+  position: absolute;
+  top: 10px;
+  right: 10px;
+  cursor: pointer;
+  border-radius: 20%;
+}
+
+.closemodal:hover {
+  background: #ccc;
+}
 </style>
